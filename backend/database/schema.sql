b682fc79b99031b8e2240af31cbd7678a8af453c
Joshua Steele <joshua.p.steele@gmail.com>
2021-12-03 20:16:47 -0500

    Saving progress on database scripts

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -20,6 +20,25 @@ CREATE TABLE users (
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-
+--
+-- DROP TABLE IF EXISTS users_games;
+-- DROP SEQUENCE IF EXISTS seq_user_game_id;
+--
+-- CREATE SEQUENCE seq_user_game_id
+--     INCREMENT BY 1
+--     NO MAXVALUE
+--     NO MINVALUE
+--     CACHE 1;
+--
+-- CREATE TABLE users_games (
+--    user_game_id int DEFAULT nextval('seq_user_game_id'::regclass) NOT NULL,
+--    user_id varchar(50) NOT NULL,
+--    password_hash varchar(200) NOT NULL,
+--    role varchar(50) NOT NULL,
+--    CONSTRAINT PK_user PRIMARY KEY (user_id)
+-- );
+--
+-- INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+-- INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 COMMIT TRANSACTION;
