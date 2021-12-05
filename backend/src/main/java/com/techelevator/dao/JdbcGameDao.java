b106c0e85cb52a7f7c7e21e690b50f19c37b5fac
Joshua Steele <joshua.p.steele@gmail.com>
2021-12-05 15:07:56 -0500

    Fill in JdbcGameDao methods

--- backend/src/main/java/com/techelevator/dao/JdbcGameDao.java
+++ backend/src/main/java/com/techelevator/dao/JdbcGameDao.java
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -14,45 +16,71 @@ public class JdbcGameDao implements GameDao {
     public JdbcGameDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
-    // TODO Fill in all methods below.
-
+    
     @Override
     public List<Game> getGames() {
-        return null;
+        List<Game> games = new ArrayList<Game>();
+        String sql = "SELECT * FROM games;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()) {
+            Game game = mapRowToGame(results);
+            games.add(game);
+        }
+        return games;
     }
 
     @Override
     public Game getGameByGameId(Long gameId) {
-        return null;
+        String sql = "SELECT * FROM games WHERE game_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, gameId);
+        return mapRowToGame(results);
     }
 
     @Override
     public Game getGameByGameOrganizer(Long gameOrganizer) {
-        return null;
+        String sql = "SELECT * from games WHERE game_organizer = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, gameOrganizer);
+        return mapRowToGame(results);
     }
 
     @Override
     public Game getGameByGameWinner(Long gameWinner) {
-        return null;
+        String sql = "SELECT * from games WHERE game_winner = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, gameWinner);
+        return mapRowToGame(results);
     }
 
     @Override
     public boolean create(Game gameToCreate) {
-        return false;
+        String sql = "INSERT INTO games (game_organizer, game_winner, start_datetime, end_datetime, game_status) " +
+                "VALUES (?, ?, ?, ?, ?);";
+        return jdbcTemplate.update(sql, gameToCreate.getGameOrganizer(), gameToCreate.getGameWinner(),
+                gameToCreate.getStartDatetime(), gameToCreate.getEndDatetime(), gameToCreate.getGameStatus()) == 1;
     }
 
     @Override
     public boolean update(Game gameToUpdate) {
-        return false;
+        String sql = "UPDATE games SET game_organizer = ?, game_winner = ?, start_datetime = ?, end_datetime = ?, " +
+                "game_status = ? WHERE game_id = ?;";
+        return jdbcTemplate.update(sql, gameToUpdate.getGameOrganizer(), gameToUpdate.getGameWinner(),
+                gameToUpdate.getStartDatetime(), gameToUpdate.getEndDatetime(), gameToUpdate.getGameStatus(),
+                gameToUpdate.getGameId()) == 1;
     }
 
     @Override
     public boolean delete(Long gameIdToDelete) {
-        return false;
+        String sql = "DELETE FROM games WHERE game_id = ?;";
+        return jdbcTemplate.update(sql, gameIdToDelete) == 1;
     }
 
     private Game mapRowToGame(SqlRowSet results) {
-        return null;
+        Game game = new Game();
+        game.setGameId(results.getLong("game_id"));
+        game.setGameOrganizer(results.getLong("game_organizer"));
+        game.setGameWinner(results.getLong("game_winner"));
+        game.setStartDatetime(results.getObject("start_datetime", LocalDateTime.class));
+        game.setEndDatetime(results.getObject("end_datetime", LocalDateTime.class));
+        game.setGameStatus(results.getString("game_status"));
+        return game;
     }
 }
