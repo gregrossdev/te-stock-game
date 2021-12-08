479a11418da0c3eaa8f418be8bd350071de864d4
Gregory Ross <gregross.dev@gmail.com>
2021-12-08 10:02:36 -0500

    format to prevent merge conflict

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,20 +1,16 @@
 <template>
   <main>
     <h1>Test Data</h1>
-    <hr/>
-    <br/>
+    <hr />
+    <br />
     <h3>Users</h3>
-    <section class="users"> 
-      <article
-        v-for="data in dataUsers"
-        v-bind:key="data.id"
-        class="user"
-      >
+    <section class="users">
+      <article v-for="data in dataUsers" v-bind:key="data.id" class="user">
         <h4>{{ data.username }}</h4>
       </article>
     </section>
     <h3>Stocks</h3>
-    <section class="stocks"> 
+    <section class="stocks">
       <article
         v-for="data in dataStocks"
         v-bind:key="data.sharePrice"
@@ -25,43 +21,33 @@
       </article>
     </section>
     <h3>Games</h3>
-    <section class="games"> 
-      <article
-        v-for="data in dataGames"
-        v-bind:key="data.gameId"
-        class="game"
-      >
+    <section class="games">
+      <article v-for="data in dataGames" v-bind:key="data.gameId" class="game">
         <h4>{{ data }}</h4>
-
       </article>
     </section>
     <h3>Portfolios</h3>
-    <section class="portfolios"> 
+    <section class="portfolios">
       <article
         v-for="data in dataPortfolios"
         v-bind:key="data"
         class="portfolio"
       >
         <h4>{{ data }}</h4>
-
       </article>
     </section>
     <h3>Transactions</h3>
-    <section class="transactions"> 
+    <section class="transactions">
       <article
         v-for="data in dataTransactions"
         v-bind:key="data"
         class="transaction"
       >
         <h4>{{ data }}</h4>
-
       </article>
     </section>
 
     <button v-on:click="updateSpce()">UPDATE SPCE</button>
-
-
-
   </main>
 </template>
 
@@ -75,9 +61,9 @@ import requestTransactions from "@/services/ServiceTransactions";
 export default {
   name: "test-data",
   methods: {
-      updateSpce() {
-        requestStocks.test();
-      }
+    updateSpce() {
+      requestStocks.test();
+    },
   },
   data() {
     return {
@@ -118,25 +104,24 @@ main {
   margin-top: 2rem;
 }
 
-
 h3 {
   font-size: 1.75rem;
   color: var(--clr-grey-10);
-  font-weight: 800; 
+  font-weight: 800;
 }
 
 h4 {
   font-size: 1.25rem;
   color: var(--clr-grey-10);
-  font-weight: 600; 
+  font-weight: 600;
 }
 
 section {
   padding: 1rem;
-  display: flex; 
+  display: flex;
   flex-wrap: wrap;
   gap: 1rem;
-  margin-bottom: 1rem;   
+  margin-bottom: 1rem;
 }
 
 .users h4 {
@@ -148,21 +133,20 @@ section {
 }
 
 .stocks {
-  border: 3px solid var(--clr-pri-30);;
+  border: 3px solid var(--clr-pri-30);
 }
 
 .games {
-  border: 3px solid var(--clr-pri-40);;
+  border: 3px solid var(--clr-pri-40);
 }
 
 .portfolios {
-  border: 3px solid var(--clr-pri-50);;
+  border: 3px solid var(--clr-pri-50);
 }
 
 .transactions {
-  border: 3px solid var(--clr-pri-60);;
+  border: 3px solid var(--clr-pri-60);
 }
-
 
 .stock {
   border: 1px solid var(--clr-grey-70);
@@ -170,9 +154,7 @@ section {
   border-radius: 0.25rem;
   padding: 0.5em;
   width: 6rem;
-  height: 6rem;   
+  height: 6rem;
 }
-
-
 </style>
 
