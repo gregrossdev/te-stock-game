a2f932a3cf41602786652ebd0e1e1b69beb8de0c
Joshua Steele <joshua.p.steele@gmail.com>
2021-12-13 16:11:28 -0500

    Finish TransactionController, including changes to schema and PortfolioDao

--- backend/src/main/java/com/techelevator/controller/TransactionController.java
+++ backend/src/main/java/com/techelevator/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.techelevator.controller;
 import com.techelevator.dao.PortfolioDao;
 import com.techelevator.dao.TransactionDao;
 import com.techelevator.model.portfolio.Portfolio;
+import com.techelevator.model.portfolioStock.PortfolioStock;
 import com.techelevator.model.transaction.Transaction;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,18 +46,28 @@ public class TransactionController {
     @RequestMapping(path = "", method = RequestMethod.POST)
     public boolean create(@RequestBody Transaction transactionToCreate) {
         Long portfolioId = transactionToCreate.getPortfolioId();
+        String stockSymbol = transactionToCreate.getStockSymbol();
         String transactionType = transactionToCreate.getTransactionType();
         BigDecimal transactionAmount = transactionToCreate.getTransactionAmount();
+        BigDecimal transactionShares = transactionToCreate.getTransactionShares();
 
         Portfolio portfolio = portfolioDao.getPortfolioByPortfolioId(portfolioId);
         BigDecimal portfolioCash = portfolio.getPortfolioCash();
 
+        PortfolioStock portfolioStock = portfolioDao.getPortfolioStockByPortfolioIdAndStockSymbol(portfolioId, stockSymbol);
+        BigDecimal portfolioShares = portfolioStock.getTotalShares();
+
         if (transactionType.equalsIgnoreCase("BUY") && portfolioCash.compareTo(transactionAmount) >= 0) {
             portfolio.setPortfolioCash(portfolioCash.subtract(transactionAmount));
+            portfolioDao.buyStock(portfolioId, stockSymbol, transactionShares);
             portfolioDao.update(portfolio);
+        } else if (transactionType.equalsIgnoreCase("SELL") && portfolioShares.compareTo(transactionShares) >= 0) {
+            portfolio.setPortfolioCash(portfolioCash.add(transactionAmount));
+            portfolioDao.sellStock(portfolioId, stockSymbol, transactionShares);
+            portfolioDao.update(portfolio);
+        } else {
+            return false;
         }
-
-
 
         return transactionDao.create(transactionToCreate);
     }
