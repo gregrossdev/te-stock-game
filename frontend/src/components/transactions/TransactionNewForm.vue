3a011514d7d3771fc6b8da9da7dd6b7b131af6bf
Joshua Steele <joshua.p.steele@gmail.com>
2021-12-13 16:59:02 -0500

    Took a stab at some code in TransactionNewForm; Please check/change as needed.

--- frontend/src/components/transactions/TransactionNewForm.vue
+++ frontend/src/components/transactions/TransactionNewForm.vue
@@ -5,10 +5,52 @@
 </template>
 
 <script>
+import serviceTransactions from "@/services/ServiceTransactions";
+
 export default {
   name: "transaction-new-form",
+  data() {
+    return {
+      transaction: {
+        portfolioId: this.$store.state.activePortfolio.portfolioId,
+        stockSymbol: "",
+        transactionType: "",
+        transactionAmount: null,
+        transactionShares: null,
+        sharePrice: this.$store.state.stocks.find(stock => stock.stockSymbol === this.stockSymbol).sharePrice,
+      }
 
-}
+// EXAMPLE OF JSON RETURN OBJECT FROM SERVER:
+// {
+//   "transactionId": 1,
+//     "portfolioId": 1,
+//     "stockSymbol": "RKLB",
+//     "transactionType": "BUY",
+//     "transactionAmount": 1000,
+//     "transactionShares": 10,
+//     "sharePrice": 100,
+//     "transactionTimestamp": "2021-12-01T18:00:00.000+00:00",
+//     "transactionStatus": "COMPLETED",
+//     "portfolioCashAfterTransaction": 99000
+// }
+
+    }
+  },
+  methods: {
+    saveTransaction() {
+      if (this.transaction.transactionAmount == null || this.transaction.transactionAmount === 0) {
+        this.transaction.transactionAmount = this.transaction.transactionShares * this.transaction.sharePrice;
+      } else if (this.transaction.transactionShares == null || this.transactionShares === 0) {
+        this.transaction.transactionShares = this.transaction.transactionAmount / this.transaction.sharePrice;
+      }
+      serviceTransactions.create(this.transaction).then((response) => {
+        if (response && response.status === 201) {
+          this.$router.push('/game/:gameId/portfolio/:portfolioId'); // NOT SURE IF THIS IS THE RIGHT WAY OR PATH TO RE-ROUTE THE URL. NEEDS TO GO BACK TO THE CURRENT/ACTIVE GAME VIEW.
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style>
