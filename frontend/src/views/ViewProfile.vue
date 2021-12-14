ba980ef0f8e5072ab3b1f5c55fa4d48dd70b541d
Gregory Ross <gregross.dev@gmail.com>
2021-12-14 16:16:16 -0500

    house keeping

--- frontend/src/views/ViewProfile.vue
+++ frontend/src/views/ViewProfile.vue
@@ -2,14 +2,10 @@
   <main class="profile">
     <h2>Welcome to the Virtual Stock Market, {{ this.$store.state.user.username }}!</h2>
     <p>Your user ID is {{ this.$store.state.user.id }}.</p>
-  
     <section>
-      <game-list></game-list>
-    
+      <game-list />
     </section>
-<!--    <section>-->
-<!--      <stock-list /> -->
-<!--    </section>-->
+
   </main>
 </template>
 
