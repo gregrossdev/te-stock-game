376a7d9175f8e0d16833f647f7f3d169a505d914
Joshua Steele <joshua.p.steele@gmail.com>
2021-12-14 14:23:56 -0500

    Added a gameId prop to GameCard.vue, as well as a created() method to set active game and set active portfolio; still need to pass gameId as a prop to GameCard from parent component

--- frontend/src/components/games/GameCard.vue
+++ frontend/src/components/games/GameCard.vue
@@ -1,12 +1,41 @@
 <template>
-    <div>
-      
-    </div>
+  <div>
+
+  </div>
 </template>
 
 <script>
+import serviceGames from "@/services/ServiceGames";
+import servicePortfolios from "@/services/ServicePortfolios";
+
 export default {
   name: "game-card",
+  props: {
+    gameId: Number // TODO: MAKE SURE PARENT COMPONENT PASSES A GAMEID TO EACH GAMECARD AS A PROP.
+  },
+  methods: {},
+  created() {
+    serviceGames
+        .getGameByGameId(this.gameId)
+        .then(response => {
+          this.$store.commit("SET_ACTIVE_GAME", response.data);
+        })
+        .catch(error => {
+          if (error.response.status === 404) {
+            this.$router.push("/"); // TODO: EITHER ADD CUSTOM ERROR MESSAGE ("GAME NOT FOUND") HERE, OR CREATE A CUSTOM 404 PAGE TO REDIRECT TO
+          }
+        });
+    servicePortfolios
+        .getPortfolioByUserIdAndGameId(this.$store.state.user.id, this.gameId)
+        .then(response => {
+          this.$store.commit("SET_ACTIVE_PORTFOLIO", response.data);
+        })
+        .catch(error => {
+          if (error.response.status === 404) {
+            this.$router.push("/"); // TODO: EITHER ADD CUSTOM ERROR MESSAGE ("GAME NOT FOUND") HERE, OR CREATE A CUSTOM 404 PAGE TO REDIRECT TO
+          }
+        });
+  }
 };
 </script>
 
