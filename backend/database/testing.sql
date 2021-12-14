31f223372e605d0f3ce37432d768a8034d6e7c97
Gregory Ross <gregross.dev@gmail.com>
2021-12-14 12:36:09 -0500

    reproduce error

---
+++ backend/database/testing.sql
@@ -0,0 +1,9 @@
+-- create new game and use that game_id
+
+INSERT INTO portfolios (user_id, game_id, portfolio_status) 
+VALUES (4, 29, 'PENDING';
+
+INSERT INTO portfolios (user_id, game_id, portfolio_status, portfolio_cash, portfolio_stocks_value, portfolio_total_value) 
+VALUES (4, 29, 'PENDING', 0, 0, 0);
+
+
