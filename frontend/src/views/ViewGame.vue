33894ad2464d5b6d843e1d512a539ceae97ffa95
Gregory Ross <gregross.dev@gmail.com>
2021-12-14 15:54:35 -0500

    start of team agreed workflow

--- frontend/src/views/ViewGame.vue
+++ frontend/src/views/ViewGame.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-      <p>game</p>
-   
+      <portfolio-list />
+      <stock-list />
   </div>
 </template>
 
 <script>
+import StockList from "@/components/stocks/StockList.vue";
+import PortfolioList from "@/components/portfolios/PortfolioList.vue";
 
 export default {
-    name: "game",
+    name: "GameId",
+    components: { 
+    StockList,
+    PortfolioList 
+ }, 
 
 }
 </script>
