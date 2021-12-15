3750c73b107ba8dacbc10faef3c9d16a8c6ef961
Gregory Ross <gregross.dev@gmail.com>
2021-12-15 10:40:18 -0500

    random image switch case

--- frontend/src/components/games/GameCard.vue
+++ frontend/src/components/games/GameCard.vue
@@ -1,37 +1,79 @@
 <template>
-  <router-link v-bind:to="{ name: 'ViewGame', params: { gameId: this.gameId } }">
+  <router-link
+    v-bind:to="{ name: 'ViewGame', params: { gameId: this.gameId } }"
+  >
     <article class="game card">
       <h2>Game #{{ this.gameId }}</h2>
-      <img src="https://source.unsplash.com/300x150/?stocks" alt="stocks" />
+      <img :src="stonksImg(url)" alt="stocks" />
     </article>
   </router-link>
 </template>
 
 <script>
-
 export default {
   name: "game-card",
+  data() {
+    return {
+      url: "https://source.unsplash.com/300x150/?"
+    }
+  },
   props: {
-    gameId: Number
+    gameId: Number,
   },
   methods: {
     viewGame(gameId) {
       this.$router.push(`/game/${gameId}`);
     },
-    randomBackgroundColor() {
-      return "#" + this.generateHexCode();
-    },
-    generateHexCode() {
-      var bg = Math.floor(Math.random()*16777215).toString(16);
-      if (bg.length !== 6) bg = this.generateHexCode();
-      return bg;
-    },
-  }
+    stonksImg(url) {
+      let num = Math.floor(Math.random() * 12);
+      let query;
+      switch (num) {
+        case 0:
+          query = "coins";
+          break;
+        case 1:
+          query = "stocks";
+          break;
+        case 2:
+          query = "money";
+          break;
+        case 3:
+          query = "cash";
+          break;
+        case 4:
+          query = "win";
+          break;
+        case 5:
+          query = "lottery";
+          break;
+        case 6:
+          query = "finance";
+          break;
+        case 7:
+          query = "currency";
+          break;
+        case 8:
+          query = "crypto";
+          break;
+        case 9:
+          query = "exchange";
+          break;
+        case 10:
+          query = "slots";
+          break;
+        case 11:
+          query = "charts";
+          break;
+        default:
+          query = "deco";
+      }
+      return url + query 
+    }
+  },
 };
 </script>
 
 <style scoped>
-
 article img {
   width: 100%;
   border: 1px solid var(--clr-grey-30);
@@ -43,7 +85,6 @@ h2 {
   padding: 0.25em 0;
   margin: 0 0 0 0.15em;
 }
-
 </style>
 
 
