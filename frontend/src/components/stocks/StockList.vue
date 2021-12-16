82c261157589ee688f5e8f7a1bf60735a50a72cc
Gregory Ross <gregross.dev@gmail.com>
2021-12-16 10:05:26 -0500

    search functionality

--- frontend/src/components/stocks/StockList.vue
+++ frontend/src/components/stocks/StockList.vue
@@ -1,35 +1,36 @@
 <template>
   <section>
+
+<!--    TODO: Add some way to FILTER and SORT the StockList. Perhaps just use StockTable with some added functionality/styling if StockCards prove too difficult to filter and sort?-->
+
     <input type="text" v-model="filterStock" />
     <div class="stock-list">
-      <article
+      <stock-card
         v-for="stock in filterStocks"
         v-bind:key="stock.stockSymbol"
-        class="stock card stock-card"
-      >
-        <h5>
-          {{ stock.stockSymbol }} |
-          {{ stock.sharePrice }}
-        </h5>
-        <p>{{ stock.stockName }}</p>
-        <transaction-new-form v-bind:sharePrice="stock.sharePrice" v-bind:stockSymbol="stock.stockSymbol"/>
-      </article>
+        class="stock card"
+        v-bind:stock="stock"
+      />
     </div>
   </section>
 </template>
 
 <script>
 import requestStocks from "@/services/ServiceStocks";
-import TransactionNewForm from "@/components/transactions/TransactionNewForm";
+import StockCard from "@/components/stocks/StockCard";
 
 export default {
   name: "stock-list",
-  components: { TransactionNewForm }, 
   data() {
     return {
       filterStock: "",
     };
   },
+
+  components: {
+    StockCard,
+  },
+
   created() {
     requestStocks.list().then((response) => {
       this.$store.commit("SET_STOCKS", response.data);
@@ -47,44 +48,12 @@ export default {
 };
 </script>
 
-
 <style scoped>
 .stock-list {
-  padding: 1.5rem 1rem;
+  padding: 1.5rem 1rem; 
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 1rem;
-}
-
-.transaction-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.transaction-form input,
-.transaction-form button {
-  margin-bottom: 0;
-  margin-top: 0.5em;
-}
-
-.hidden {
-  display: none;
-}
-
-.stock-card {
-  color: white;
-  border: 1px solid var(--clr-grey-70);
-  margin-bottom: 1em;
-  border-radius: 0.25rem;
-  padding: 0.5em;
-  width: 10rem;
-  display: flex;
-  flex-direction: column;
-}
-
-.stock-card p {
-  color: white;
-  font-size: 1em;
 }
 </style>
